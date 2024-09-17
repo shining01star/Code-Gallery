4b0fee8e11d3a6381365afbb6e96f737fef43670
Shining01Star <www.sakshi530@gmail.com>
2024-09-17 15:01:09 +0530

    Code-Gallery

---
+++ Rock-Paper-Scissor Game.py
@@ -0,0 +1,29 @@
+import random
+
+options = ("rock", "paper", "scissors")
+winning = ("scissors", "rock", "paper")
+running = True
+
+while running:
+
+    player = None
+    computer = random.choice(options)
+
+    while player not in options:
+        player = input("Enter a choice (rock, paper, scissors): ").lower()
+
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
+
+    if player == computer:
+        print("It's a tie!")
+    elif options.index(player) == winning.index(computer):
+        print("You win !")
+    else:
+        print("You lose!")
+
+    if not input("Play again? (y/n): ").lower() == "y":
+        running = False
+
+print("Thanks for playing!")
+
